7c7a84047e3d985620436e6fa98b6d4d9855adae
kevin.serrano <kevin.serrano@famoco.com>
2016-08-08 22:17:31 +0200

    initial commit sources

---
+++ HouseCleaningPlanner/app/src/main/java/com/example/kevin/housecleaningplanner/AbstractCleaningTask.java
@@ -0,0 +1,11 @@
+package com.example.kevin.housecleaningplanner;
+
+/**
+ * Created by kevin on 8/8/16.
+ */
+public abstract class AbstractCleaningTask implements CleaningTaskInterface {
+
+    private String name;
+    private double timesPerWeek;
+
+}
---
+++ HouseCleaningPlanner/app/src/main/java/com/example/kevin/housecleaningplanner/CleaningTaskInterface.java
@@ -0,0 +1,17 @@
+package com.example.kevin.housecleaningplanner;
+
+/**
+ * Created by kevin on 8/8/16.
+ */
+public interface CleaningTaskInterface {
+
+    void displayTask();
+
+    void finishTask();
+
+    void snoozeTask();
+
+    boolean isSnoozable();
+
+
+}
